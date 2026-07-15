115fd7a0ec6dc50ba08d065ab6be64e132912fc8
ハビエルシェンマシュルイズ <habierushen@gmail.com>
2026-07-15 13:08:52 +0900

    Add files via upload

---
+++ FileSphere v.2/archivefile.php
@@ -0,0 +1,72 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: auth.html");
+    exit();
+}
+
+include "includes/database.php";
+
+$username = $_SESSION['username'];
+
+if (!isset($_GET['id'])) {
+    die("No file selected.");
+}
+
+$fileId = intval($_GET['id']);
+
+/* Verify ownership */
+
+$stmt = mysqli_prepare(
+    $conn,
+    "SELECT id
+     FROM files
+     WHERE id = ?
+     AND owner = ?"
+);
+
+mysqli_stmt_bind_param(
+    $stmt,
+    "is",
+    $fileId,
+    $username
+);
+
+mysqli_stmt_execute($stmt);
+
+$result = mysqli_stmt_get_result($stmt);
+
+if (mysqli_num_rows($result) == 0) {
+    die("You do not have permission to archive this file.");
+}
+
+/* Archive */
+
+$stmt = mysqli_prepare(
+    $conn,
+    "UPDATE files
+     SET archived = 1
+     WHERE id = ?
+     AND owner = ?"
+);
+
+mysqli_stmt_bind_param(
+    $stmt,
+    "is",
+    $fileId,
+    $username
+);
+
+if (mysqli_stmt_execute($stmt)) {
+
+    header("Location: filemanager.php");
+    exit();
+
+} else {
+
+    die("Database Error: " . mysqli_error($conn));
+
+}
+?>
---
+++ FileSphere v.2/deleteforever.php
@@ -0,0 +1,62 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['username'])) {
+    header("Location: auth.html");
+    exit();
+}
+
+include "includes/database.php";
+
+$username=$_SESSION['username'];
+
+$id=intval($_GET['id']);
+
+$stmt=mysqli_prepare(
+$conn,
+"SELECT filename
+FROM files
+WHERE id=?
+AND owner=?
+AND deleted=1"
+);
+
+mysqli_stmt_bind_param(
+$stmt,
+"is",
+$id,
+$username
+);
+
+mysqli_stmt_execute($stmt);
+
+$result=mysqli_stmt_get_result($stmt);
+
+if($row=mysqli_fetch_assoc($result)){
+
+$file="uploads/".$row['filename'];
+
+if(file_exists($file)){
+unlink($file);
+}
+
+$stmt=mysqli_prepare(
+$conn,
+"DELETE FROM files
+WHERE id=?"
+);
+
+mysqli_stmt_bind_param(
+$stmt,
+"i",
+$id
+);
+
+mysqli_stmt_execute($stmt);
+
+}
+
+header("Location:trash.php");
+
+?>
